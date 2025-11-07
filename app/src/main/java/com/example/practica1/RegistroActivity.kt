cd7dffce44f504adbe5d5ee03d2bbe671c66406f
alumno <ayoubbenjaddi5@gmail.com>
2025-11-07 13:54:05 +0100

    Practica1

---
+++ app/src/main/java/com/example/practica1/RegistroActivity.kt
@@ -0,0 +1,41 @@
+package com.example.practica1
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registro)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+
+        val boton=findViewById<Button>(R.id.webbutton)
+        boton.setOnClickListener {
+            val url="https://github.com/Peluas9000/Kotlin_Zenobia"
+            val intent= Intent(Intent.ACTION_VIEW, Uri.parse(url))
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
